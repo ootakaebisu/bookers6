5671e505ec67b7e3ca8d0732e05bc5c3176b61c5
EC2 Default User <ec2-user@ip-172-31-23-201.ec2.internal>
2020-10-11 16:59:57 +0000

    [stop]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+  end
+end
+
+
+def autheniticate_user
+  if @current_user==nil
+    flash[:notice]="ログインが必要です"
+    redirect_to("/")
+  end
 end
